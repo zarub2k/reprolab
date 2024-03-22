4b1e90ea8aa0e93e9e2449ae863e6c8b92eef6c6
Tham <zarub2k@gmail.com>
2024-03-22 17:11:12 +0530

    clean up is done

--- src/test/java/xyz/tham/OlingoPayloadTest.java
+++ src/test/java/xyz/tham/OlingoPayloadTest.java
@@ -1,8 +1,7 @@
 package xyz.tham;
 
-
 import org.apache.olingo.client.api.ODataClient;
-import org.apache.olingo.client.api.domain.ClientEntity;
+import org.apache.olingo.client.api.domain.ClientEntitySet;
 import org.apache.olingo.client.api.serialization.ODataDeserializerException;
 import org.apache.olingo.client.core.ODataClientFactory;
 import org.apache.olingo.commons.api.format.ContentType;
@@ -11,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.time.Duration;
 import java.time.Instant;
 
@@ -26,19 +24,17 @@ public class OlingoPayloadTest {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("olingo_payload.json").getFile());
         try {
-//            FileReader fileReader = new FileReader(file);
             FileInputStream inputStream = new FileInputStream(file);
             ODataClient oDataClient = ODataClientFactory.getClient();
             Instant start = Instant.now();
-//            long start = System.currentTimeMillis();
-            oDataClient.getReader().readEntitySet(inputStream, ContentType.APPLICATION_JSON);
+
+            ClientEntitySet entitySet = oDataClient.getReader().readEntitySet(inputStream, ContentType.APPLICATION_JSON);
+            System.out.println("Entries size: " + entitySet.getEntities().size());
+
             Instant finish = Instant.now();
             long timeTaken = Duration.between(start, finish).toSeconds();
+
             System.out.println("Time taken: " + timeTaken);
-//            ClientEntity clientEntity = ODataClientFactory.getClient().getReader().readEntity(inputStream,
-//                    ContentType.APPLICATION_JSON);
-//            long end = System.currentTimeMillis();
-//            System.out.println("Time taken: " + (end - start));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (ODataDeserializerException e) {
